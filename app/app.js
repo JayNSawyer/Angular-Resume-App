ab024a401248b36fb600cc878452f5c4ab97af6c
Jay Sawyer <jnaeem31@gmail.com>
2015-05-27 22:18:20 -0400

    create initial api and bootstrap angular

---
+++ app/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('resume', ['ngResource', 'ngRoute'])
+	.controller('AppCtrl', [
+		'$scope',
+		'$injector',
+		function ($scope, $injector){
+			var $rootScope = $injector.get('$rootScope'),
+				$http = $injector.get('$http');
+
+			var self = this;	
+
+			$scope.greeting = 'Welcome to the Resume App!';
+
+			this.getUsers = function(){
+				return $http.get('/api/users').then(function(data){			
+					return data.data;
+				}, function(error){
+					console.log(error);
+				});
+			};
+
+			this.getUsers().then(function(data){
+				$scope.users = data;
+			});
+		}
+	]);
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 
+var users = [
+	{
+		id: 0,
+		first_name: 'Jimmy',
+		last_name: 'Jones'
+	},
+	{
+		id: 1,
+		first_name: 'Tommy',
+		last_name: 'Thompson'
+	},
+	{
+		id: 2,
+		first_name: 'Sammy',
+		last_name: 'Smith'
+	}
+];
+
+/* Template */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', users: users });
 });
+
+
+/* JSON API */
+
+router.get('/api/users', function(req, res, next){
+  res.json(users);
+});
+
 
 module.exports = router;
