090aedc8d2bad6d2ebe705786c8ac915a1cbf010
Jay Sawyer <jnaeem31@gmail.com>
2015-12-25 01:57:33 -0500

    Load bower js and css dependencies automagically into main index file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,38 @@
-(function() {
+(function () {
     'use strict';
 
     var gulp = require('gulp');
     var args = require('yargs').argv;
     var config = require('./gulp.config')();
 
-    var $gulp = require('gulp-load-plugins')({lazy: true}); //$gulp will automagically load node module names of 'gulp' and 'gulp-'
 
-    gulp.task('vet', function() {
+    var $gulp = require('gulp-load-plugins')({
+        lazy: true
+    }); //$gulp will automagically load node module names of 'gulp' and 'gulp-'
+
+    var wiredep = require('wiredep').stream;
+
+    gulp.task('build', function () {
         return gulp.src(config.srcFiles)
             .pipe($gulp.if(args.verbose, $gulp.print()))
             .pipe($gulp.jscs())
             .pipe($gulp.jshint())
-            .pipe($gulp.jshint.reporter('jshint-stylish', {verbose: true}));
+            .pipe($gulp.jshint.reporter('jshint-stylish', {
+                verbose: true
+            }));
+    });
+
+    gulp.task('inject', function () {
+
+        var options = {
+            bowerJson: require('./bower.json'),
+            directory: './vendor',
+            ignorePath: '../vendor'
+        };
+        
+        return gulp.src('./views/*.ejs')
+            .pipe(wiredep(options))
+            .pipe(gulp.dest('./views'));
     });
 
 })();
