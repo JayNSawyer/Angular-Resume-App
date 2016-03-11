79c5ef3ec5e5e255b7eff30914da6b083cdb06d1
Jay Sawyer <jnaeem31@gmail.com>
2016-03-11 18:31:12 -0500

    Add spec for hash service

--- server/test/createUserServiceSpec.js
+++ server/test/createUserServiceSpec.js
@@ -18,14 +18,14 @@ let req = {
 };
 
 describe('createUserService', () => {
-	it('should return a new user from the request body', (done) => {
+	it('returns a new user from the request body', (done) => {
 		createUser(req.body).then((user) => {
 			expect(user.firstname).to.equal('Joe');
 			done();
 		});
 	});
 	
-	it('should hash the user password', (done) => {
+	it('hashes the user password', (done) => {
 		createUser(req.body).then((user) => {
 			expect(user.password).to.equal(undefined);
 			expect(user.salt).to.be.ok;
@@ -34,7 +34,7 @@ describe('createUserService', () => {
 		});
 	});
 	
-	it('should call the setSalt method once', (done) => {
+	it('calls the setSalt method once', (done) => {
 		let setSalt = sinon.spy(HashService, 'setSalt');
 		createUser(req.body).then((user) => {
 			expect(setSalt.called).to.be.true;
@@ -42,7 +42,7 @@ describe('createUserService', () => {
 		});
 	});
 	
-	it('should call the createHash method once', (done) => {
+	it('calls the createHash method once', (done) => {
 		let createHash = sinon.spy(HashService, 'createHash');
 		createUser(req.body).then((user) => {
 			expect(createHash.called).to.be.true;
---
+++ server/test/hashServiceSpec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let chai = require('chai');
+let expect = chai.expect;
+let sinon = require('sinon');
+
+let HashService = require('../services/hashService');
+
+
+let salt = '$2a$10$5eyaPZln8WikoFkLoeU0Ve';
+let password = 'MyPassword';
+let hash = '$2a$10$5eyaPZln8WikoFkLoeU0Ve3anvMRQvpFBKQK2TvY3hfuo3dLoj/vm';
+
+
+describe('HashService', () => {
+	it('generates a salt', (done) => {
+		HashService.setSalt().then((salt) => {
+			expect(salt).to.be.a('string');
+			expect(salt).to.have.length.of.at.least(10);
+			done();
+		});
+	});
+
+	it('creates a hash from a password and salt', (done) => {
+		HashService.createHash(password, salt).then((hash) => {
+			expect(hash).to.be.a('string');
+			expect(hash).to.have.length.of.at.least(10);
+			expect(hash).to.not.equal(password);
+			done();
+		});
+	});
+
+	it('calls comparePasswordToHash and returns true if password matches hash', (done) => {
+		HashService.comparePasswordToHash(password, hash).then((res) => {
+			expect(res).to.be.true;
+			done();
+		});
+	});
+
+});
